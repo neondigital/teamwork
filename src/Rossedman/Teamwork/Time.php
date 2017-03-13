d3b577e417f4f38cc63942cd98d5d341b7b0f997
Glenn <glenn@neondigital.co.uk>
2017-03-13 16:56:02 +0000

    Removing restriction of options on time entries

--- src/Rossedman/Teamwork/Time.php
+++ src/Rossedman/Teamwork/Time.php
@@ -10,15 +10,4 @@ class Time extends AbstractObject {
 
     protected $endpoint = 'time_entries';
 
-    /**
-     * GET /time_entries.json
-     *
-     * @return mixed
-     */
-    public function all($args = null)
-    {
-        $this->areArgumentsValid($args, ['page']);
-
-        return $this->client->get($this->endpoint, $args)->response();
-    }
 }
